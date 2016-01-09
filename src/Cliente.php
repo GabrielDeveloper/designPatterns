827c2c9cc58b0c904b4f3f3bbf8865b89cb3ecd1
Gabriel Goncalves <gabrielll_07@hotmail.com>
2016-01-09 02:31:59 -0200

    Criando teste para cobrir classe Conta

--- src/Cliente.php
+++ src/Cliente.php
@@ -5,7 +5,8 @@ namespace App;
 class Cliente extends Conta
 {
 
-    public function __construct(){
+    public function __construct()
+    {
         return true;
     }
 }
--- tests/unit/ClienteTest.php
+++ tests/unit/ClienteTest.php
@@ -10,26 +10,33 @@ class ClienteTeste extends PHPUnit_Framework_TestCase
 
     public function testRetornaModelo()
     {
-        $cliente = new Cliente;
+        $cliente = new \App\Cliente;
         $this->assertTrue($cliente != null);
     }
 
-    public function testRetornaSaldo()
+    public function testDepositaERetornaSaldoValido()
     {
+        $cliente = new Cliente;
+        $cliente->depositar(4);
+        $this->assertEquals($cliente->getSaldo(), 4);
+    }
 
+    public function testDeveDepositarESacarERetornarSaldoExato()
+    {
+        $cliente = new Cliente;
+        $cliente->depositar(10);
+        $cliente->sacar(5);
+        $this->assertEquals($cliente->getSaldo(), 5);
+    }
+
+    public function testMockingObject()
+    {
         $mock = $this->getMock('Cliente', ['save'], []);
 
         $mock->expects($this->any())
                 ->method('save')
                 ->will($this->returnValue(true));
 
-        $mock->depositar(4);
-
-        $this->assertTrue($mock->save());
-
-        $cliente = new Cliente;
- //       $cliente->depositar(4);
-//        $this->assertEquals($cliente->getSaldo(), 4);
     }
 
 }
