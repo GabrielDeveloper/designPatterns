4d897f7baa896b941c17f1f6577fe1447a2fdee4
Gabriel Goncalves <gabrielll_07@hotmail.com>
2016-01-15 01:05:49 -0200

    Criando diretorio para estudar SOLID

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ include_once('vendor/autoload.php');
 
 use App\PHPUnit\Cliente;
 use App\Factory\FactoryCar;
+use App\Solid\SingleResponsibillity\Pedido;
 
 $cliente = new Cliente();
 
 
 $car = FactoryCar::createCar("Astra", 2002);
 echo $car->getinformation();
+
+$pedido = new Pedido();
 
